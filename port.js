fbb606abc59f53d969bf6c68a1f6eed326ba04cf
abrorbekx27 <133501107+abrorbekx27@users.noreply.github.com>
2023-09-04 22:29:37 +0500

    Delete port.js

--- port.js
+++
@@ -1,8 +0,0 @@
-var typed= new typed((".multiple-text");{
-  strings:["Brand Representative", "Sales Associate","Multilingual Candidate"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop:true
-}
-)
